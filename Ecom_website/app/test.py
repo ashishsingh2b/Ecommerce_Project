1e9bf3f2d2fef1d875be57ccd987ebcc2fe07f0f
ashishsingh2b <ashishsingh2b@gmail.com>
2024-12-03 18:10:33 +0530

    new bugs

--- Ecom_website/app/test.py
+++ Ecom_website/app/test.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 
 def myname(request):
     return HttpResponse("Hello, world!")
+
+def admindashboard(request):
+    return HttpResponse("Welcome to Admin Dashboard")
+def userdahsboard(request):
+    return HttpResponse("Welcome to User Dashboard")
